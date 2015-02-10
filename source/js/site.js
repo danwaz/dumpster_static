6de3a379fc05381e7d346b755d3df7f97da6486b
Dan Wasilewski <heydanwaz@gmail.com>
2015-02-10 14:18:33 -0500

    removed link if no num is input to search

--- source/js/site.js
+++ source/js/site.js
@@ -3,11 +3,8 @@ $(function(){
 		submit : function (e) {
 			e.preventDefault();
 			var dest = $('.idNum').val();
-			console.log(dest.length);
 			if(dest.length > 0) {
 				window.location.href = '/items/' + dest;
-			} else {
-				window.location.href = '/items/321';
 			}
 		}
 	});
